d2fea30cb1880d56b4c685641ccaeb9764d6ed0c
ming <ming@diningcity.aisa>
2016-08-08 10:47:33 +0800

    add  NOTIFICATION

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,2 +1,4 @@
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 ActiveJob::Base.logger = Logger.new((File.join(Rails.root, 'log', 'active_job.log')))
+
+NOTIFICATION_KEY = ENV["AUTH_ACCESS_KEY"]
