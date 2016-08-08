f9c5906e146a728615b388673503f6f7fe4de712
ming <ming@diningcity.aisa>
2016-08-08 16:26:41 +0800

    remove

---
+++ app/jobs/send_notification_job.rb
@@ -0,0 +1,29 @@
+class SendNotificationJob < ApplicationJob
+  queue_as :default
+
+  def perform(tokens,msg,link=nil,scheduled=nil)
+    # Do something later
+    url = URI("https://api.ionic.io/push/notifications")
+
+    send_object = {tokens: tokens.split(',') ,profile: 'production',notification: {message: msg}}
+
+    if scheduled && Time.parse(scheduled)
+      send_object[:scheduled]= DateTime.parse(scheduled).rfc3339
+    end
+    if link
+      send_object[:notification]= {payload: {my_data: link}}
+    end
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(url)
+    request["authorization"] =  ENV["AUTH_ACCESS_KEY"]
+    request["content-type"] = 'application/json'
+    request["cache-control"] = 'no-cache'
+    request.body =send_object.to_json
+
+    response = http.request(request)
+    puts response.read_body
+  end
+end
