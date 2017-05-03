dde7e9e57a9fdcfd4793196d40b8729a9646404c
ming <zhangming1021@gmail.com>
2017-05-03 14:01:36 +0800

    add web console

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  require "sidekiq/web"
+  mount Sidekiq::Web, at: "/sidekiq"
+
   resources :app_nms,only: :create
   namespace :restaurant do
     resources :app_nms,only: :create
